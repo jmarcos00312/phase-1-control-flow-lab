d285b2ab4836edf6cdc608a5bc08caa221677aaf
Jeremiah <jeremiahmarcos27@outlook.com>
2021-10-13 08:21:08 -0500

    commit tree 78e84a55404089e596916b68ad6b97b12e24a760

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!"
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city) {
+  return (city === "NYC")? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous) {
+  switch (generous) {
+     case "generous": 
+    return "Thank you so much.";
+    case "not as generous":
+    return "Thank you.";
+    default:
+    return "Bye."
+  }
+  }
+  
   // Write your code here!
-}
